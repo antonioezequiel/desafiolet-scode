447315fc128bddaaab4f3201334c9d59823bf059
antonio.ezequiel <antonio.ezequiel@ufpe.br>
2022-03-14 17:46:46 -0300

    terceiro commite

--- src/main/java/com/letscode/moviebattle/moviebattle/controller/LoginController.java
+++ src/main/java/com/letscode/moviebattle/moviebattle/controller/LoginController.java
@@ -3,15 +3,16 @@ package com.letscode.moviebattle.moviebattle.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 
 import com.letscode.moviebattle.moviebattle.classes.LoginForm;
 import com.letscode.moviebattle.moviebattle.classes.DTO.TokenDTO;
@@ -25,7 +26,7 @@ public class LoginController {
 	@Autowired
 	TokenService tokenService;
 
-	@PostMapping
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> logar(@RequestBody @Valid LoginForm form) {
 		UsernamePasswordAuthenticationToken loginDados = form.converterDados();
 		try {
